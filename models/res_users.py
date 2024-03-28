786558bfb4fe1a0d36c08e2fb8c192e3df872c9a
Myke Rodas <mikealquimia@gmail.com>
2024-03-28 17:37:26 -0600

    fix production_area in res_users

--- models/res_users.py
+++ models/res_users.py
@@ -5,5 +5,4 @@ from odoo import models, fields, api
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    #production_area_id = fields.Many2one('production.area', string="Area de trabajo en producción")
-    hola = fields.Boolean()
+    production_area_id = fields.Many2one('production.area', string="Area de trabajo en producción")
